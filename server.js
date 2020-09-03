071f5471b9bf96a6b3c78a32f75b4dffc61bc45c
sachin-budni <sachinswapna143@gmail.com>
2020-09-03 17:44:35 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/rohitnlp'));
+// Start the app by listening on the default
+// Heroku port
+app.use('/**', express.static(__dirname + '/dist/rohitnlp/index.html'));
+app.listen(process.env.PORT || 8080);
